395310dc7b71f770e1d91d9ec217adad89b6c79c
Krishna Addepalli <coolkrishna31@gmail.com>
2017-09-07 10:28:45 +0530

    Removed unnecessary transformations, and also corrected the Json output format being written out.

--- Scala/src/main/scala/TwitterStream.scala
+++ Scala/src/main/scala/TwitterStream.scala
@@ -1,8 +1,6 @@
 /**
   * Code from this site: https://mapr.com/blog/spark-streaming-and-twitter-sentiment-analysis/
-  *
-  */
-
+*/
 import java.sql.Timestamp
 import org.apache.spark._
 import org.apache.spark.streaming.twitter.TwitterUtils
@@ -25,41 +23,37 @@ object TwitterStream extends App {
       "replace the key and secret keys with appropriate twitter hash values.")
   }
 //  else {
-    val config = new SparkConf().setAppName("twitter-stream-sentiment-krishna")
-      .setMaster("local[3]")
-      .set("spark.executor.memory", "1g")
-      .set("spark.driver.allowMultipleContexts", "true")
+  val config = new SparkConf().setAppName("twitter-stream-sentiment-krishna")
+    .setMaster("local[3]")
+    .set("spark.executor.memory", "1g")
+    .set("spark.driver.allowMultipleContexts", "true")
 
-    val sc = new SparkContext(config)
-    sc.setLogLevel("WARN")
-    val s = new StreamingContext(sc, Seconds(2))
-    //  s.checkpoint("/media/sf_D_DRIVE/TwitterStreamData/")
-    s.checkpoint("D:/TwitterStreamData/")
+  val sc = new SparkContext(config)
+  sc.setLogLevel("WARN")
+  val s = new StreamingContext(sc, Seconds(2))
+  //  s.checkpoint("/media/sf_D_DRIVE/TwitterStreamData/")
+  s.checkpoint("D:/TwitterStreamData/")
 
-      System.setProperty("twitter4j.oauth.consumerKey", args(0))
-      System.setProperty("twitter4j.oauth.consumerSecret", args(1))
-      System.setProperty("twitter4j.oauth.accessToken", args(2))
-      System.setProperty("twitter4j.oauth.accessTokenSecret", args(3))
+  System.setProperty("twitter4j.oauth.consumerKey", args(0))
+  System.setProperty("twitter4j.oauth.consumerSecret", args(1))
+  System.setProperty("twitter4j.oauth.accessToken", args(2))
+  System.setProperty("twitter4j.oauth.accessTokenSecret", args(3))
 
-    val stream = TwitterUtils.createStream(s, None)
-    stream.count.print
+  val stream = TwitterUtils.createStream(s, None)
+  stream.count.print
 
-    implicit val formats = Serialization.formats(NoTypeHints)
+  implicit val formats = Serialization.formats(NoTypeHints)
 
-    val formatter = DateTimeFormatter.ofPattern("dd_MM_yyyy-HH_mm_ss")
+  val formatter = DateTimeFormatter.ofPattern("dd_MM_yyyy-HH_mm_ss")
 
-    stream.map(s => Tweet(s.getText, s.getUser.getName,
-      s.getId, new Timestamp(s.getCreatedAt.getTime),
-      s.getRetweetCount, s.getUser.getLocation, s.getLang))
-      .map(write(_))
-      .reduce(_+ "\n"+ _)
-      .foreachRDD{s =>
-        val now = LocalDateTime.now
-        Files.write(Paths.get(s"D:/TwitterStreamData/${formatter.format(now)}.json"),
-          s.collect.foldLeft("")((z, a) => z+a+"\n")
-            .getBytes(StandardCharsets.UTF_8)) }
-    s.start
-    s.awaitTermination
-//  }
-
+  stream.map(s => Tweet(s.getText, s.getUser.getName,
+    s.getId, new Timestamp(s.getCreatedAt.getTime),
+    s.getRetweetCount, s.getUser.getLocation, s.getLang))
+    .foreachRDD { t =>
+      val now = LocalDateTime.now
+      Files.write(Paths.get(s"D:/TwitterStreamData/${formatter.format(now)}.json"),
+                  write(t.collect).getBytes(StandardCharsets.UTF_8))
+  }
+  s.start
+  s.awaitTermination
 }
