d32411ae3ccaef0127d3b517ff009d00890b7240
murali <muralibilla@gmail.com>
2017-09-21 17:39:31 +0530

    Generating Preliminary Graph..

--- Python/Spark_Python.py
+++ Python/Spark_Python.py
@@ -5,19 +5,39 @@ https://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
 
 from tweepy import Stream, OAuthHandler
 from tweepy.streaming import StreamListener
+from threading import Thread
+import multiprocessing
 import collections
 import datetime
+import time
+import matplotlib.pyplot  as plt
+
 
 Tweet = collections.namedtuple('Tweet', 'text user id createdAt retweetCount location language')
 
 class MYListener(StreamListener):
+    def __init__(self):
+        self.x = []
+        self.y = []
+
     def on_data(self, data):
         try:
-            #time = datetime.datetime()
-            #time.
-            with open(time.strftime('%Y%m%d-%H%M%S') + '.json', 'a') as f:
+            plt.xlabel('python count')
+            plt.ylabel('time stamp')
+            plt.grid(True)
+            iter = 0;
+            with open('Python_1.json', 'a') as f:
                 f.write(data)
-                return True
+                find_word = '#python'
+                self.y.append(data.count(find_word))
+                ts = time.time()
+                st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                self.x.append(st)
+            # print (self.x, self.y)
+            plt.xticks(range(0, len(self.x)), self.x)
+            plt.bar(range(0, len(self.x)), self.y)
+            plt.show()
+            return True
         except BaseException as e:
             print("Error on_data: " + str(e))
         return True
@@ -38,9 +58,11 @@ if __name__=='__main__':
     consumerKey, consumerSecret, accessToken, accessTokenSecret = \
                 (sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
+    # thread = Thread(target=plot)
+    # thread.setDaemon(True)
+    # thread.start()
     auth = OAuthHandler(consumerKey, consumerSecret)
     auth.set_access_token(accessToken, accessTokenSecret)
-    #auth.
 
     twitter_stream= Stream(auth, MYListener())
     twitter_stream.filter(track=['#python'])
